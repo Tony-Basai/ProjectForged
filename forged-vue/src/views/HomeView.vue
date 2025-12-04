0b1e01a21e6d17418d3a87a5cf4c37c42b90ba70
unknown <User@DESKTOP-T9A3RFT.mk.local>
2025-12-04 19:44:21 +0300

    debug

--- forged-vue/src/views/HomeView.vue
+++ forged-vue/src/views/HomeView.vue
@@ -10,6 +10,6 @@
 <script setup>
 import AppHero from '../components/AppHero.vue';
 import AppAboutUs from '../components/AppAboutUs.vue';
-import AppOurProducts from '../components/AppOurproducts.vue'
+import AppOurProducts from '../components/AppOurProducts.vue'
 import AppPhotoGallery from '@/components/AppPhotoGallery.vue';
 </script>
